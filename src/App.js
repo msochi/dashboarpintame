d38464c7cef6d59560890e9c74db1b3829536dcc
Marcelo S <marcelo.sochi@gmail.com>
2021-04-17 23:15:24 -0300

    dash

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './assets/css/style.css';
+import Dash from "./components/body.js"
+
+
+
+function App() {
+
+  
+  return (
+    
+    <div className="App">
+    <Dash/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/cuerpodash.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Menusuperior from "./menuspuerior";
+import Graficosdash from "./graficosdash";
+import ProductosEnStock from "./productosEnStock";
+
+function Cuerpodash() {
+  return (
+    <div id="content">
+
+        <Menusuperior/>
+        <Graficosdash/>
+      
+
+    </div>    
+  
+
+
+
+
+
+   
+  );
+}
+
+export default Cuerpodash;
